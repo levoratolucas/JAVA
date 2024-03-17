84dbe310672ec691cc00b58dc069893c0051fe4a
Lucas Levorato <lucaslevorato@gmail.com>
2024-03-17 14:27:16 -0300

    exe13 check

--- Exe13.java
+++ Exe13.java
@@ -1,40 +1,47 @@
 // Exe13.java
 // 13. Escreva um algoritmo que leia valores REAIS nas variáveis A e B e o tipo de 
 // operador em outra variável do tipo CARACTERE. Imprima o resultado da operação 
-
+// de A por B se o operador aritmético for válido; caso contrário deve ser impresso uma 
+// mensagem de operador não definido. Tratar erro de divisão por zero. 
 
 public class Exe13 {
-    public static void executar(){
-        Tools.println("\n\n\n13. Escreva um algoritmo que leia valores REAIS nas variáveis A e B e o tipo de operador em outra variável do tipo CARACTERE. Imprima o resultado da operação\n\n\n");
+    public static void executar() {
+        Tools.println(
+                "\n\n\n13. Escreva um algoritmo que leia valores REAIS nas variáveis A e B e o tipo de operador em outra variável do tipo CARACTERE. Imprima o resultado da operação\n\n\n");
         Tools.println("digite o primeiro numero");
         float a = Tools.scanffloat();
         Tools.println("digite o segundo numero");
         float b = Tools.scanffloat();
         Tools.println("digite o operador");
-        String op = Tools.scanfStr(),msg;
+        String op = Tools.scanfStr(), msg;
 
         switch (op) {
             case "+":
-            msg = ("Soma entre "+a+" e "+b+" = "+(a+b));
+                msg = ("Soma entre " + a + " e " + b + " = " + (a + b));
                 break;
             case "-":
-            msg = ("subtracao entre "+a+" e "+b+" = "+(a-b));
+                msg = ("subtracao entre " + a + " e " + b + " = " + (a - b));
                 break;
             case "*":
-            msg = ("multiplicacao entre "+a+" e "+b+" = "+(a*b));
+                msg = ("multiplicacao entre " + a + " e " + b + " = " + (a * b));
                 break;
             case "/":
-            msg = ("divisao entre "+a+" e "+b+" = "+(a/b));
+                while (b == 0) {
+                    Tools.println("indivisivel por ZERO");
+                    Tools.println("\ndigite o segundo numero novamente");
+                    b = Tools.scanffloat();
+                }
+                msg = ("divisao entre " + a + " e " + b + " = " + (a / b));
                 break;
             case "%":
-            msg = ("resto entre "+a+" e "+b+" = "+(a%b));
+                msg = ("resto entre " + a + " e " + b + " = " + (a % b));
                 break;
-        
+
             default:
-             msg = "digite valores validos";
+                msg = "operador invalido";
                 break;
         }
         Tools.println(msg);
     }
-    
+
 }
