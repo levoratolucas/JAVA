05cee5fc643f6fde5af51d2a8b4d76356f6d032e
Lucas Levorato <lucaslevorato@gmail.com>
2024-03-17 12:27:20 -0300

    exe4 check

--- Exe4.java
+++ Exe4.java
@@ -1,7 +1,17 @@
 // 4. Escreva um algoritmo que leia dois números e ao final mostre a soma, subtração,multiplicação e a divisão dos números lidos.
 
 public class Exe4 {
-    public static void Executar(){
+    public static void executar(){
+        int cont = 2;
+        float  soma = 0, subtracao = 0 , multiplicacao = 1, divisao = 1;
+        float[] nums = new float[cont];
+
+        for (int i = 0; i < cont; i++ ) {
+            nums[i]= Tools.scanffloat();            
+            Tools.println("este é "+ nums[i]);   
+            soma += nums[i];        
+        } 
+        Tools.printFormat("soma é %.2f%n", soma);
         
     }
 }
--- Tools.java
+++ Tools.java
@@ -5,10 +5,17 @@ public class Tools {
     public static void println(String msg){
         System.out.println(msg);
     }
+    public static void printFormat(String format, Object... args){
+        System.out.printf(format, args);
+    }
     
     public static int scanfInt() {
         Scanner scanner = new Scanner(System.in);
         return scanner.nextInt();
+    }
+    public static float scanffloat() {
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextFloat();
     }
     public static String scanfStr() {
         Scanner scanner = new Scanner(System.in);
